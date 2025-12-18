b17b40bcb7c43d760d601bf93777d1f9efacde4c
RENO ROY <seenaroy81@gmail.com>
2025-12-18 12:10:49 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "https://chatiappp.onrender.com/api",
   withCredentials: true,
 });
 
